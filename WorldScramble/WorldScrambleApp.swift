d4b3b2e3a4bfa579421708be5b9989c4954e196d
TarasKarabas <taraskyparenko@gmail.com>
2023-04-12 07:33:09 +1030

    Initial Commit

---
+++ WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Taras Kyparenko on 12/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
